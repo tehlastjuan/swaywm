b721e33864ee55160f00214c63ca6127d0aa312f
Juan Rodrigo <juanrodr100@student.kau.se>
2026-06-16 10:40:23 +0200

    updated sway/brightness

--- .local/share/bash_lib/sway/brightness.sh
+++ .local/share/bash_lib/sway/brightness.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 # shellcheck disable=1091,2034
 
-source /usr/local/bin/userenv --
-source "$BASH_LIB/sway/wobctl.sh"
+BASH_LIB="$XDG_DATA_HOME/bash_lib"
 
 FACTOR=2
 CURRENT_ABS=$(brightnessctl get)
 MAX=$(brightnessctl max)
 BRIGHTNESS_STEP=$((MAX * FACTOR / 100 < 1 ? 1 : MAX * FACTOR / 100))
 
-current_brightness() {
+function current_brightness() {
   echo "$(brightnessctl get) * 100 / $(brightnessctl max)" | bc
 }
 
@@ -19,10 +18,42 @@ current_brightness() {
 #   # pkill --uid "$USER" --newest "/home/juanro/.local/share/bash_lib/sway/brightness.sh"
 # }
 
-brightness() {
+function brightness() {
   case "${1-}" in
     --get)
-      current_brightness
+      shift
+      case "${1-}" in
+        --json)
+          # format
+          # {"text": "$text", "alt": "$alt", "tooltip": "$tooltip", "class": "$class", "percentage": $_percentage }"
+          local _alt _json _cur_brightness
+          _cur_brightness=$(current_brightness)
+
+          if [[ "$_cur_brightness" -gt 0 && "$_cur_brightness" -lt 9 ]]; then _alt=10; fi
+          if [[ "$_cur_brightness" -gt 10 && "$_cur_brightness" -lt 19 ]]; then _alt=20; fi
+          if [[ "$_cur_brightness" -gt 20 && "$_cur_brightness" -le 29 ]]; then _alt=30; fi
+          if [[ "$_cur_brightness" -gt 30 && "$_cur_brightness" -le 39 ]]; then _alt=40; fi
+          if [[ "$_cur_brightness" -gt 40 && "$_cur_brightness" -le 49 ]]; then _alt=50; fi
+          if [[ "$_cur_brightness" -gt 50 && "$_cur_brightness" -le 59 ]]; then _alt=60; fi
+          if [[ "$_cur_brightness" -gt 60 && "$_cur_brightness" -le 69 ]]; then _alt=70; fi
+          if [[ "$_cur_brightness" -gt 70 && "$_cur_brightness" -le 79 ]]; then _alt=80; fi
+          if [[ "$_cur_brightness" -gt 80 && "$_cur_brightness" -le 89 ]]; then _alt=90; fi
+          if [[ "$_cur_brightness" -gt 90 && "$_cur_brightness" -le 100 ]]; then _alt=100; fi
+
+          _json=$(cat << EOT
+{
+  "text": "brightness",
+  "alt": "${_cur_brightness}",
+  "tooltip": "Brightness: ${_cur_brightness}%",
+  "class": "brightness",
+  "percentage": ${_cur_brightness}
+}
+EOT
+)
+          printf '%s' "$_json" | jq --unbuffered --compact-output
+          ;;
+        *) current_brightness ;;
+      esac
       ;;
     --down)
       # if current value <= 2% and absolute value != 1, set brightness to absolute 1
@@ -30,17 +61,15 @@ brightness() {
         brightnessctl --quiet set 1
       else
         brightnessctl --quiet set "${BRIGHTNESS_STEP}-"
-        wobctl --show "$(current_brightness)"
       fi
     ;;
     --up)
       brightnessctl --quiet set "${BRIGHTNESS_STEP}+"
-      wobctl --show "$(current_brightness)"
       ;;
     *) exit 0 ;;
   esac
 }
 
-if [[ "${#BASH_SOURCE[@]}" -eq 1 ]]; then
+if [ "${#BASH_SOURCE[@]}" -eq 1 ]; then
   brightness "$@"
 fi
